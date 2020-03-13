51ca695269cced2850676d53e10007b06b028a64
selimcanozdemir <ozdemirselimcan@protonmail.com>
2020-03-13 16:57:39 +0300

    git

---
+++ precache-manifest.961469475370cb11c760d8fdfab15674.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134ab41de4f37d611ccf0f1c6ef52e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17933a92253308399c26",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "97cc50521c818b31ad0b",
+    "url": "/static/css/main.9b05f5b9.chunk.css"
+  },
+  {
+    "revision": "17933a92253308399c26",
+    "url": "/static/js/2.640de764.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.640de764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97cc50521c818b31ad0b",
+    "url": "/static/js/main.eb0a3381.chunk.js"
+  },
+  {
+    "revision": "c4a289f730dfd726852d",
+    "url": "/static/js/runtime-main.566c4248.js"
+  },
+  {
+    "revision": "795dba8ccafa2b653e6d648fba73a45f",
+    "url": "/static/media/Brown-Bold.795dba8c.ttf"
+  },
+  {
+    "revision": "91d670e591c7aeed73ad632454005bb3",
+    "url": "/static/media/Brown-Light.91d670e5.ttf"
+  },
+  {
+    "revision": "456fa5f28d7b513ea9165dd0e728d92d",
+    "url": "/static/media/Brown-Regular.456fa5f2.ttf"
+  },
+  {
+    "revision": "70c63884bea54efde6e0becbcaf88d49",
+    "url": "/static/media/clouds.70c63884.png"
+  },
+  {
+    "revision": "252f12f9fa8c98e74919672476fd29b6",
+    "url": "/static/media/logo.252f12f9.svg"
+  },
+  {
+    "revision": "0e92819357c7ef35cdd3d9e0ec7fcaa6",
+    "url": "/static/media/mountain.0e928193.jpg"
+  }
+]);
